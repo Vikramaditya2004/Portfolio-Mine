fbb56cca5e4b718d5db5e059903de5fd52839afa
Vikram <Sudhanshu.1996.vk@gmail.com>
2026-09-17 12:22:03 +0530

    Update section

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-// const menuToggle = document.querySelector(".menu-toggle");
-// const nav = document.querySelector("nav");
-// const navLinks = document.querySelectorAll("nav a");
-
-// menuToggle.addEventListener("click", () => {
-//   const isOpen = nav.classList.toggle("active");
-//   menuToggle.setAttribute("aria-expanded", isOpen);
-//   menuToggle.setAttribute(
-//     "aria-label",
-//     isOpen ? "Close navigation menu" : "Open navigation menu",
-//   );
-//   menuToggle.innerHTML = isOpen
-//     ? '<i class="fa-solid fa-xmark"></i>'
-//     : '<i class="fa-solid fa-bars"></i>';
-// });
-
-// navLinks.forEach((link) => {
-//   link.addEventListener("click", () => {
-//     nav.classList.remove("active");
-//     menuToggle.setAttribute("aria-expanded", "false");
-//     menuToggle.setAttribute("aria-label", "Open navigation menu");
-//     menuToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
-//   });
-// });
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
 
     const headerContainer = document.getElementById("header");
